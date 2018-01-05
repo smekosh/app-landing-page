54d5abfabd3b739cb8ec8ee34e4806eb3f8bb706
Stephen Mekosh <smekosh@users.noreply.github.com>
2018-01-05 15:25:51 -0500

    Switched all news feed items to show images and teasers

--- inc/news-feed.php
+++ inc/news-feed.php
@@ -10,93 +10,134 @@
 
 <div class="wrapper news-feed">
 	
-	<div class="featured-news">
+	<a href="" class="news-item">
 		
-		<a href="" class="news-item featured-news-item">
-			
-			<div class="news-img">
-				<img src="https://gdb.voanews.com/F826A707-1558-497E-9325-2DB788E49D9B_w200_r1_s.jpg" />
-			</div>
-			
-			<div class="news-text">
-				<h3>即时博客：伊朗抗议持续进行</h3>
-				<p>Lorem Ipsum teaser text if needed.</p>
-			</div>
-			
-		</a>
+		<div class="news-img">
+			<img src="https://gdb.voanews.com/F826A707-1558-497E-9325-2DB788E49D9B_w200_r1_s.jpg" />
+		</div>
 		
-		<a href="" class="news-item featured-news-item">
-			
-			<div class="news-img">
-				<img src="https://gdb.voanews.com/F826A707-1558-497E-9325-2DB788E49D9B_w200_r1_s.jpg" />
-			</div>
-			
-			<div class="news-text">
-				<h3>川普要求DACA法案必须纳入资助美墨界墙和取消连锁移民与绿卡抽签</h3>
-				<p>Lorem Ipsum teaser text if needed.</p>
-			</div>
-			
-		</a>
+		<div class="news-text">
+			<h3>即时博客：伊朗抗议持续进行</h3>
+			<p>Lorem Ipsum teaser text if needed.</p>
+		</div>
 		
-		<a href="" class="news-item featured-news-item">
-			
-			<div class="news-img">
-				<img src="https://gdb.voanews.com/F826A707-1558-497E-9325-2DB788E49D9B_w200_r1_s.jpg" />
-			</div>
-			
-			<div class="news-text">
-				<h3>即时博客：伊朗抗议持续进行</h3>
-				<p>Lorem Ipsum teaser text if needed.</p>
-			</div>
-			
-		</a>
-		
-	</div><!-- end .featured-news -->
+	</a>
 	
-	<div class="older-news">
+	<a href="" class="news-item">
 		
-		<a href="" class="news-item">
-			<div class="news-text">
-				<h3>即时博客：伊朗抗议持续进行</h3>
-			</div>
-		</a>
+		<div class="news-img">
+			<img src="https://gdb.voanews.com/F826A707-1558-497E-9325-2DB788E49D9B_w200_r1_s.jpg" />
+		</div>
 		
-		<a href="" class="news-item">
-			<div class="news-text">
-				<h3>川普要求DACA法案必须纳入资助美墨界墙和取消连锁移民与绿卡抽签</h3>
-			</div>
-		</a>
+		<div class="news-text">
+			<h3>川普要求DACA法案必须纳入资助美墨界墙和取消连锁移民与绿卡抽签</h3>
+			<p>Lorem Ipsum teaser text if needed.</p>
+		</div>
 		
-		<a href="" class="news-item">
-			<div class="news-text">
-				<h3>即时博客：伊朗抗议持续进行</h3>
-			</div>
-		</a>
+	</a>
+	
+	<a href="" class="news-item">
 		
-		<a href="" class="news-item">
-			<div class="news-text">
-				<h3>即时博客：伊朗抗议持续进行</h3>
-			</div>
-		</a>
+		<div class="news-img">
+			<img src="https://gdb.voanews.com/F826A707-1558-497E-9325-2DB788E49D9B_w200_r1_s.jpg" />
+		</div>
 		
-		<a href="" class="news-item">
-			<div class="news-text">
-				<h3>即时博客：伊朗抗议持续进行</h3>
-			</div>
-		</a>
+		<div class="news-text">
+			<h3>即时博客：伊朗抗议持续进行</h3>
+			<p>Lorem Ipsum teaser text if needed.</p>
+		</div>
 		
-		<a href="" class="news-item">
-			<div class="news-text">
-				<h3>川普要求DACA法案必须纳入资助美墨界墙和取消连锁移民与绿卡抽签</h3>
-			</div>
-		</a>
+	</a>
+	
+	<a href="" class="news-item">
 		
-		<a href="" class="news-item">
-			<div class="news-text">
-				<h3>即时博客：伊朗抗议持续进行</h3>
-			</div>
-		</a>
+		<div class="news-img">
+			<img src="https://gdb.voanews.com/F826A707-1558-497E-9325-2DB788E49D9B_w200_r1_s.jpg" />
+		</div>
 		
-	</div><!-- end .
+		<div class="news-text">
+			<h3>即时博客：伊朗抗议持续进行</h3>
+			<p>Lorem Ipsum teaser text if needed.</p>
+		</div>
+		
+	</a>
+	
+	<a href="" class="news-item">
+		
+		<div class="news-img">
+			<img src="https://gdb.voanews.com/F826A707-1558-497E-9325-2DB788E49D9B_w200_r1_s.jpg" />
+		</div>
+		
+		<div class="news-text">
+			<h3>川普要求DACA法案必须纳入资助美墨界墙和取消连锁移民与绿卡抽签</h3>
+			<p>Lorem Ipsum teaser text if needed.</p>
+		</div>
+		
+	</a>
+	
+	<a href="" class="news-item">
+		
+		<div class="news-img">
+			<img src="https://gdb.voanews.com/F826A707-1558-497E-9325-2DB788E49D9B_w200_r1_s.jpg" />
+		</div>
+		
+		<div class="news-text">
+			<h3>即时博客：伊朗抗议持续进行</h3>
+			<p>Lorem Ipsum teaser text if needed.</p>
+		</div>
+		
+	</a>
+	
+	<a href="" class="news-item">
+		
+		<div class="news-img">
+			<img src="https://gdb.voanews.com/F826A707-1558-497E-9325-2DB788E49D9B_w200_r1_s.jpg" />
+		</div>
+		
+		<div class="news-text">
+			<h3>即时博客：伊朗抗议持续进行</h3>
+			<p>Lorem Ipsum teaser text if needed.</p>
+		</div>
+		
+	</a>
+	
+	<a href="" class="news-item">
+		
+		<div class="news-img">
+			<img src="https://gdb.voanews.com/F826A707-1558-497E-9325-2DB788E49D9B_w200_r1_s.jpg" />
+		</div>
+		
+		<div class="news-text">
+			<h3>川普要求DACA法案必须纳入资助美墨界墙和取消连锁移民与绿卡抽签</h3>
+			<p>Lorem Ipsum teaser text if needed.</p>
+		</div>
+		
+	</a>
+	
+	<a href="" class="news-item">
+		
+		<div class="news-img">
+			<img src="https://gdb.voanews.com/F826A707-1558-497E-9325-2DB788E49D9B_w200_r1_s.jpg" />
+		</div>
+		
+		<div class="news-text">
+			<h3>即时博客：伊朗抗议持续进行</h3>
+			<p>Lorem Ipsum teaser text if needed.</p>
+		</div>
+		
+	</a>
+	
+	<a href="" class="news-item">
+		
+		<div class="news-img">
+			<img src="https://gdb.voanews.com/F826A707-1558-497E-9325-2DB788E49D9B_w200_r1_s.jpg" />
+		</div>
+		
+		<div class="news-text">
+			<h3>川普要求DACA法案必须纳入资助美墨界墙和取消连锁移民与绿卡抽签</h3>
+			<p>Lorem Ipsum teaser text if needed.</p>
+		</div>
+		
+	</a>
 	
 </div><!-- end .news-feed -->
