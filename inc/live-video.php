a1c5a6b6145a25c5fd53f6be581e56d5a9011e86
pp19dd <pp19dd@gmail.com>
2018-01-09 10:09:03 -0500

    Rewrite JS tracking logic

--- inc/live-video.php
+++ inc/live-video.php
@@ -11,25 +11,48 @@
 
 <script>
 // bandwidth saver: click to replace youtube thumbnail with video iframe
+// placeholder click event loads iframe, loaded iframe loads YT / GTM tracking api
+// player click -> load_video -> load_tracker
 (function() {
 	var player = document.getElementById('yt-embed-container');
 
-	function has_iframe() {
-		var e = document.querySelector('#yt-embed-container iframe');
+	function has_placeholder() {
+		var e = document.querySelector('div.yt-placeholder');
 		if( e === null ) {
 			return( false );
 		}
 		return( true );
 	}
 
+	var thatscript = document.currentScript;
+
+	function load_tracker() {
+		var gtm_s = document.createElement("script");
+		gtm_s.type = "text\/javascript";
+		thatscript.parentNode.insertBefore(gtm_s, thatscript);
+		gtm_s.src = "https://www.youtube.com/iframe_api";
+	}
+
+	function load_video() {
+		player.innerHTML = "";
+
+		var yt_i = document.createElement("iframe");
+		yt_i.frameborder = "0";
+		yt_i.gesture = "media";
+		yt_i.allow = "encrypted-media";
+		yt_i.allowfullscreen = "";
+		yt_i.onload = function() {
+			load_tracker();
+		}
+
+		player.appendChild(yt_i);
+		yt_i.src = "https://www.youtube.com/embed/<?php echo $youtube->id->videoId ?>?&autoplay=1";
+	}
+
 	player.addEventListener("click", function() {
-		if( has_iframe() !== false ) return;
-		player.innerHTML =
-			// triggers gtm event?
-			"<script src='https://www.youtube.com/iframe_api'><" + "/script>" +
-			// autoplays since user already intended to watch video
-			"<iframe src='https://www.youtube.com/embed/<?php echo $youtube->id->videoId ?>?&autoplay=1' frameborder='0' gesture='media' allow='encrypted-media' allowfullscreen></iframe>"
-		;
+		if( has_placeholder() === false ) return;
+
+		load_video();
 	});
 })();
 </script>
