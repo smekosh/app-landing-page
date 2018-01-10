928a30d15f94a068a137ef3f83c224bc0fe289ef
Stephen Mekosh <smekosh@users.noreply.github.com>
2018-01-10 11:06:21 -0500

    Added meta title, dates, robots and commented out unused meta tags
    Serialized stylesheet with date-time of last edit

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-W72N2D');</script>
 <!-- End Google Tag Manager -->
 
-	<title></title>
+	<title><?php echo $loc['meta_title']; ?></title>
 
 	<link type="image/x-icon" rel="icon" href="<?php echo BASE_URL; ?>/img/favicon.ico" />
 	<link rel="shortcut icon" href="<?php echo BASE_URL; ?>/img/favicon.ico" />
@@ -27,35 +27,37 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	<link rel="canonical" href="<?php echo BASE_URL; ?>/" />
 	<meta name="standout" content="<?php echo BASE_URL; ?>/" />
 	<meta name="description" itemprop="description" content="" />
-	<link rel="image_src" href="" />
+	<?php /* <link rel="image_src" href="" />
 	<meta name="keywords" content="" />
-	<meta name="news_keywords" content="" />
+	<meta name="news_keywords" content="" /> */ ?>
 
 	<meta property="og:site_name" content="<?php echo $loc['meta_og_site_name']; ?>" />
 	<meta property="og:type" content="article" />
 	<meta property="og:url" content="<?php echo BASE_URL; ?>/" />
-	<meta property="og:title" content="" />
-	<meta property="og:description" content="" />
+	<meta property="og:title" content="<?php echo $loc['meta_title']; ?>" />
+	<?php /* <meta property="og:description" content="" />
 	<meta property="og:image" content="" />
 	<meta property="og:image:width" content="1275" />
-	<meta property="og:image:height" content="600" />
+	<meta property="og:image:height" content="600" /> */ ?>
 
 	<meta name="twitter:site" value="@<?php echo $loc['meta_twitter_creator']; ?>" />
 	<meta name="twitter:creator" content="@<?php echo $loc['meta_twitter_creator']; ?>" />
 	<meta name="twitter:card" value="summary" />
 	<meta name="twitter:url" content="<?php echo BASE_URL; ?>/" />
-	<meta name="twitter:title" content="" />
-	<meta name="twitter:description" content="" />
-	<meta name="twitter:image" content="" />
+	<meta name="twitter:title" content="<?php echo $loc['meta_title']; ?>" />
+	<?php /* <meta name="twitter:description" content="" />
+	<meta name="twitter:image" content="" /> */ ?>
 
-	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>/css/base.css?rand=<?php echo time(); ?>" />
+	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>/css/base.css?v=20180109-1358" />
 
 	<!-- documentation at https://polyfill.io/v2/docs/ -->
 	<script src="https://cdn.polyfill.io/v2/polyfill.min.js"></script>
 
-	<meta name="DISPLAYDATE" content="December 00, 2017" />
-	<meta itemprop="dateModified" content="2017-12-29" />
-	<meta itemprop="datePublished" content="2017-12-29" />
+	<meta name="DISPLAYDATE" content="<?php echo date('F j, Y'); ?>" />
+	<meta itemprop="dateModified" content="<?php echo date('Y-m-d'); ?>" />
+	<meta itemprop="datePublished" content="<?php echo date('Y-m-d'); ?>" />
+	
+	<meta name="robots" content="noindex, follow" />
 
 </head>
 
